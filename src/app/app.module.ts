dbb4541e11fa06387ff9581a704f6028d0d17892
shreyanshi <paliwalshreyanshi@gmail.com>
2023-10-03 16:53:15 +0530

    sh1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditformComponent } from './editform/editform.component';
+import { UserformComponent } from './userform/userform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditformComponent,
+    UserformComponent
   ],
   imports: [
     BrowserModule
