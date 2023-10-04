cc25e98753677066bfffd3bb13e6115f40c20e74
rajveer2345 <rajveershekhar.singh@gmail.com>
2023-10-04 10:17:01 +0530

    rajveer merge1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BlogComponent } from './blog/blog.component';
 import { RouterModule } from '@angular/router';
@@ -11,28 +10,37 @@ import { RouterModule } from '@angular/router';
 
 import { EditformComponent } from './editform/editform.component';
 import { UserformComponent } from './userform/userform.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> 46ada325adbb56032743c632264aa744a011c94c
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
     DashboardComponent,
     BlogComponent,
- EditformComponent,
-    UserformComponent
-=======
+    EditformComponent,
+    UserformComponent,
     HomeComponent
->>>>>>> 46ada325adbb56032743c632264aa744a011c94c
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'blog', component: BlogComponent},
-      {path: 'userform', component: UserformComponent},
+      {path: 'dashboard', component: DashboardComponent,
+      children: [
+        {
+          path:'blog', component: BlogComponent
+        },
+        {
+          path:'userform', component: UserformComponent
+        },
+        {
+          path:'editform', component: EditformComponent
+        }
+      ]},
+      {path: 'login', component: LoginComponent},
+      {path: 'home', component: HomeComponent},
+
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
     ]),
   ],
   providers: [],
